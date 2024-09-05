10f0e650d1b4da9b8f6ee6c68cb60014825ab9a5
Leodomi <leodomi15@gmail.com>
2024-09-05 15:32:10 -0400

    protegiendo las rutas del crud de bebidas

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->group(function(){
     Route::get('/bebidas/search', [BebidaController::class, 'searchBebidas']);
     Route::post('/logout',[AuthController::class,'logout']);
+    Route::apiResource('/bebidas', BebidaController::class);
 })->middleware('auth:sanctum');
-
-
-Route::apiResource('/bebidas', BebidaController::class);
-
 //Autenticacion de usuarios
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
